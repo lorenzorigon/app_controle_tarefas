e2d9e02f477ff66f257712abf16a44d9358964d1
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-13 09:50:46 -0300

    Listando tarefas com paginação

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -21,7 +21,9 @@ class TarefaController extends Controller
      */
     public function index()
     {
-        //
+        $user_id = auth()->user()->id;
+        $tarefas = Tarefa::where('user_id', $user_id)->paginate(1);
+        return view ('tarefa.index', ['tarefas' => $tarefas]);
     }
 
     /**
