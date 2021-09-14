b554f9bcd46cc32ede8a181ac4ab2eb84d860452
Lorenzo <lorenzocardoso@hotmail.com>
2021-09-14 09:21:44 -0300

    Adicionando estilos ao pdf

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,20 +1,54 @@
-<h2>Lista de Tarefas</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html">
+    <title>lista_de_tarefas</title>
+    <style>
+        .titulo{
+            border: 1px;
+            background-color: #c2c2c2;
+            text-align: center;
+            width: 100%;
+            text-transform: uppercase;
+            font-weight: bold;
+            margin-bottom: 25px;
+        }
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Tarefa</th>
-            <th>Data limite conclusão</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($tarefas as $tarefa)
+        table{
+            width: 100%;
+        }
+
+        table th{
+            text-align: left;
+        }
+
+        .page-break{
+            page-break-after: always;
+        }
+    </style>
+</head>
+<body>
+    <div class="titulo">Lista de Tarefas</div>
+
+    <table>
+        <thead>
             <tr>
-                <td>{{$tarefa->id}}</td>
-                <td>{{$tarefa->tarefa}}</td>
-                <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao))}}</td>
+                <th>ID</th>
+                <th>Tarefa</th>
+                <th>Data limite conclusão</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($tarefas as $tarefa)
+                <tr>
+                    <td>{{$tarefa->id}}</td>
+                    <td>{{$tarefa->tarefa}}</td>
+                    <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao))}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
